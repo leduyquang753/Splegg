3f2dc7708a7d655a8bdbd656e54359d4f5ec7d4f
Le Duy Quang <leduyquang753@gmail.com>
2017-08-07 15:37:32 +0700

    Register listeners

--- splegg/src/main/java/io/github/leduyquang753/splegg/Main.java
+++ splegg/src/main/java/io/github/leduyquang753/splegg/Main.java
@@ -8,6 +8,9 @@ public final class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		getCommand("gun").setExecutor(new CommListener());
+		getServer().getPluginManager().registerEvents(new Shoot(), this);
+		getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
 		getLogger().info("The Splegg engine test plugin has been successfully enabled.");
 		_instance = this;
 	}
