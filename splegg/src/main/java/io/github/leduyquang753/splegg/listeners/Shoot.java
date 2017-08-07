6b0d15ed942db4485cdebe6cf8458f81ad49f2e2
Le Duy Quang <leduyquang753@gmail.com>
2017-08-07 21:52:33 +0700

    Upgrade the engine

--- splegg/src/main/java/io/github/leduyquang753/splegg/listeners/Shoot.java
+++ splegg/src/main/java/io/github/leduyquang753/splegg/listeners/Shoot.java
@@ -4,13 +4,14 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.entity.Egg;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.BlockIterator;
-import org.bukkit.util.Vector;
 
 public final class Shoot implements Listener {
 	@SuppressWarnings("deprecation")
@@ -18,10 +19,11 @@ public final class Shoot implements Listener {
 	public void onPlayerUse(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		if (player.getItemInHand().getType() == Material.IRON_BARDING) {
-			Egg bullet = player.getWorld().spawn(player.getLocation(), Egg.class);
-			bullet.setVelocity(player.getLocation().getDirection());
+			Egg bullet = player.getWorld().spawn(player.getEyeLocation(), Egg.class);
+			bullet.setVelocity(player.getEyeLocation().getDirection());
+			bullet.setVelocity(bullet.getVelocity().multiply(2));
 			bullet.setShooter(player);
-			player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EGG_THROW, 1, 1);
+			player.getWorld().playSound(player.getEyeLocation(), Sound.ENTITY_EGG_THROW, 1, 0);
 		}
 	}
 	
@@ -30,6 +32,7 @@ public final class Shoot implements Listener {
 	public void onEggHit(ProjectileHitEvent event) {
 		BlockIterator iterator = new BlockIterator(event.getEntity().getWorld(), event.getEntity().getLocation().toVector(), event.getEntity().getVelocity().normalize(), 0.0D, 4);
 		Block hit = null;
+		boolean isTNT = false;
 		
 		while(iterator.hasNext()) {
 			hit = iterator.next();
@@ -38,7 +41,16 @@ public final class Shoot implements Listener {
 				break;
 			}
 		}
-		hit.getWorld().playSound(hit.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1, 1);
-		hit.setType(Material.AIR);
+		if (event.getEntity().getType() == EntityType.EGG) {
+			hit.getWorld().playSound(hit.getLocation(), Sound.ENTITY_CHICKEN_EGG, 1, 0);
+			if (hit.getType() == Material.TNT) {
+				isTNT = true;
+			}
+			hit.setType(Material.AIR);
+			if (isTNT) {
+				TNTPrimed tnt = (TNTPrimed) hit.getWorld().spawn(hit.getLocation(), TNTPrimed.class);
+				tnt.setFuseTicks(0);
+			}
+		}
 	}
 }
