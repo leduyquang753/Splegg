aa64a88329b23added84e85c3218644315fc8811
Le Duy Quang <leduyquang753@gmail.com>
2017-08-07 21:50:45 +0700

    Update Main.java

--- splegg/src/main/java/io/github/leduyquang753/splegg/Main.java
+++ splegg/src/main/java/io/github/leduyquang753/splegg/Main.java
@@ -1,16 +1,27 @@
 package io.github.leduyquang753.splegg;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import io.github.leduyquang753.splegg.listeners.GriefPrevention;
+import io.github.leduyquang753.splegg.listeners.CommListener;
+import io.github.leduyquang753.splegg.listeners.PlayerJoin;
+import io.github.leduyquang753.splegg.listeners.Shoot;
 
 public final class Main extends JavaPlugin {
 	
 	private static Main _instance;
+	public Plugin plugin;
 	
 	@Override
 	public void onEnable() {
+		this.plugin = plugin;
 		getCommand("gun").setExecutor(new CommListener());
-		getServer().getPluginManager().registerEvents(new Shoot(), this);
-		getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+		Bukkit.getPluginManager().registerEvents(new PlayerJoin(), this);
+		Bukkit.getPluginManager().registerEvents(new Shoot(), this);
+		Bukkit.getPluginManager().registerEvents(new GriefPrevention(), this);
+		
 		getLogger().info("The Splegg engine test plugin has been successfully enabled.");
 		_instance = this;
 	}
